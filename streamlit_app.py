22f425757931063190cd1d187c111a16cf7d67c4
xiaowenao <36755430+xiaowenao@users.noreply.github.com>
2024-11-21 11:59:11 -0600

    Update streamlit_app.py
    add request API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,10 @@ if ingredients_list:
 
         st.success('Your Smoothis is ordered,'+' '+name_on_order+'!' ,icon='✅')
 
+#new section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
 
